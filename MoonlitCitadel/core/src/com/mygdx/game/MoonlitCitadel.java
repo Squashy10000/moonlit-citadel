1922c06ecb227d9d0a8d8476542f26aaebd981a5
Squashy <timmytimmy641@gmail.com>
2018-09-14 10:03:18 -0400

    screens are closer to being active

--- MoonlitCitadel/core/src/com/mygdx/game/MoonlitCitadel.java
+++ MoonlitCitadel/core/src/com/mygdx/game/MoonlitCitadel.java
@@ -3,15 +3,41 @@ package com.mygdx.game;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+import Screens.CreditScreen;
+import Screens.GameScreen;
+import Screens.LoadScreen;
+import Screens.MenuScreen;
 
 public class MoonlitCitadel extends Game {
 	SpriteBatch batch;
 	Texture img;
 
+    private LoadScreen loadScreen;
+    private MenuScreen menuScreen;
+    private GameScreen gameScreen;
+    private CreditScreen creditScreen;
 
+    public enum SCREENTYPE{
+        LOAD, MENU, GAME, CREDITS
+    }
+
+    public void createScreen(SCREENTYPE type){
+        Screen screen = null;
+        switch(type){
+            case GAME:
+            case LOAD:
+                if(loadScreen == null){
+
+                }
+            case MENU:
+            case CREDITS:
+        }
+    }
 	
 	@Override
 	public void create () {
