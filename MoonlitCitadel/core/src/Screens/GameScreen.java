9d13860e0258d9eb3b0faf5efee84b7fd0e90a71
Squashy <timmytimmy641@gmail.com>
2018-10-05 10:04:09 -0400

    box 2d

--- MoonlitCitadel/core/src/Screens/GameScreen.java
+++ MoonlitCitadel/core/src/Screens/GameScreen.java
@@ -1,8 +1,14 @@
 package Screens;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.World;
 import com.mygdx.game.MoonlitCitadel;
 
 public class GameScreen implements Screen {
@@ -10,10 +16,24 @@ public class GameScreen implements Screen {
     private SpriteBatch batch;
     private Texture img;
     private MoonlitCitadel game;
+    //box2d
+    private World world;
+    private Body body;
+    private Vector2 gravity;
+    //view
+    private OrthographicCamera camera;
+    private Box2DDebugRenderer b2dr;
 
     public GameScreen(MoonlitCitadel game, SpriteBatch batch) {
         this.batch = batch;
         this.game = game;
+
+        gravity = new Vector2(0,-9.8f);
+        world = new World(gravity, false);
+        b2dr = new Box2DDebugRenderer();
+        camera = new OrthographicCamera();
+
+        camera.setToOrtho(false, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
     }
 
     public static final String TAG = GameScreen.class.getSimpleName();
