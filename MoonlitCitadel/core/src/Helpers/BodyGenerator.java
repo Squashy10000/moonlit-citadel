deaca8fed8c3886b9b128e14a30c6bfd380b0b33
Squashy <timmytimmy641@gmail.com>
2018-10-29 10:01:58 -0400

    ashley getting done, yo

--- MoonlitCitadel/core/src/Helpers/BodyGenerator.java
+++ MoonlitCitadel/core/src/Helpers/BodyGenerator.java
@@ -1,5 +1,12 @@
 package Helpers;
 
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.CircleShape;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.Shape;
 import com.badlogic.gdx.physics.box2d.World;
 
 public class BodyGenerator {
@@ -7,4 +14,53 @@ public class BodyGenerator {
     public BodyGenerator(World world){
         this.world = world;
     }
+
+
+    public Body createBody(Vector2 position, float size, float force, BodyDef.BodyType type, int bodyType, short self, short interactions){
+        Body body;
+        BodyDef bdef = new BodyDef();
+        FixtureDef fdef = new FixtureDef();
+
+        switch (type) {
+            case StaticBody:
+                bdef.type = BodyDef.BodyType.StaticBody;
+                break;
+            case DynamicBody:
+                bdef.type = BodyDef.BodyType.DynamicBody;
+                break;
+            case KinematicBody:
+                bdef.type = BodyDef.BodyType.KinematicBody;
+                break;
+        }
+        bdef.gravityScale = force;
+        bdef.position.set(position.x, position.y);
+        body = world.createBody(bdef);
+        Shape shape;
+        switch (bodyType){
+            case 0:
+                shape = new CircleShape();
+                shape.setRadius(size/2);
+                break;
+            case 1:
+                shape = new PolygonShape();
+                ((PolygonShape)shape).setAsBox(size/2, size/2);
+                break;
+            default:shape = new CircleShape();
+                shape.setRadius(size/2);
+        }
+
+
+        fdef.shape = shape;
+        fdef.density = 1f;
+        fdef.restitution = .75f;
+        fdef.filter.categoryBits = self;
+        fdef.filter.maskBits = interactions;
+        body.createFixture(fdef);
+
+        shape.dispose();
+
+        return body;
+
+    }
 }
+
